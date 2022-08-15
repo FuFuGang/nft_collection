667ff7f8fa7f6fef350277d748441e6d869d6467
FuFuGang <ganggang0422@163.com>
2022-08-15 12:19:12 +0800

    nft collection

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xdC34206EcfD03b962CB1eF6B4b0e4A1e8d83E0f8";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
